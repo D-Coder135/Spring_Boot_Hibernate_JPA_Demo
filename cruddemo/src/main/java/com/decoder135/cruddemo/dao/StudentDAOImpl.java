73adf16cde417a4689c0798cd022d5255a6c9f27
D-Coder135 <devansh1352000@gmail.com>
2023-04-01 15:45:18 +0530

    add the @Repository annotation for the class StudentDAOImpl.java class

--- cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAOImpl.java
+++ cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAOImpl.java
@@ -1,8 +1,11 @@
 package com.decoder135.cruddemo.dao;
 
 import com.decoder135.cruddemo.entity.Student;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class StudentDAOImpl implements StudentDAO{
+
     @Override
     public void save(Student theStudent) {
 
