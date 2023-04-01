3dce7104063d33f749cf18e2372d80651c2bb267
D-Coder135 <devansh1352000@gmail.com>
2023-04-01 15:20:10 +0530

    generate the getter and setter methods for all the private fields

--- cruddemo/src/main/java/com/decoder135/cruddemo/entity/Student.java
+++ cruddemo/src/main/java/com/decoder135/cruddemo/entity/Student.java
@@ -35,5 +35,38 @@ public class Student {
 
     //    define getter/setter
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 //    define toString() method
 }
