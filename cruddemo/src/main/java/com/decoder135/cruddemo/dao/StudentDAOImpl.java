9e767764637aadeacc7981ee89c72a976942d158
D-Coder135 <devansh1352000@gmail.com>
2023-04-01 15:44:54 +0530

    override the required methods inside the StudentDAOImpl.java class

--- cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAOImpl.java
+++ cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAOImpl.java
@@ -1,4 +1,10 @@
 package com.decoder135.cruddemo.dao;
 
+import com.decoder135.cruddemo.entity.Student;
+
 public class StudentDAOImpl implements StudentDAO{
+    @Override
+    public void save(Student theStudent) {
+
+    }
 }
