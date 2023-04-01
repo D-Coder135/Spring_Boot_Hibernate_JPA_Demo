dd54dd1d2057e8ceb8ef01755d4a4e4c9959efe7
D-Coder135 <devansh1352000@gmail.com>
2023-04-01 16:07:31 +0530

    update the argument for the commandLineRunner() method

--- cruddemo/src/main/java/com/decoder135/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/decoder135/cruddemo/CruddemoApplication.java
@@ -1,5 +1,6 @@
 package com.decoder135.cruddemo;
 
+import com.decoder135.cruddemo.dao.StudentDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +14,7 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
 			System.out.println("Command Line Application Using SpringBoot.");
 		};
