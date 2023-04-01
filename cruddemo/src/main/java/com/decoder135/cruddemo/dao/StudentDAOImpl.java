93fd6513732cf81ab20837bc50857b7333a2efdc
D-Coder135 <devansh1352000@gmail.com>
2023-04-01 15:46:39 +0530

    define a private field of type EntityManager

--- cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAOImpl.java
+++ cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAOImpl.java
@@ -1,12 +1,14 @@
 package com.decoder135.cruddemo.dao;
 
 import com.decoder135.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
 
     // define field for entity manager
+    private EntityManager entityManager;
 
     // inject entity manager using constructor injection
 
