1e32f6f3cc6b8ae32ba38bb889a08266baed14a7
D-Coder135 <devansh1352000@gmail.com>
2023-04-02 13:06:25 +0530

    define a method findAll() of type List<Student>

--- cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAO.java
+++ cruddemo/src/main/java/com/decoder135/cruddemo/dao/StudentDAO.java
@@ -2,9 +2,13 @@ package com.decoder135.cruddemo.dao;
 
 import com.decoder135.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
